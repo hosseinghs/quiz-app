287b46c53a53e0e1fc8fe2058dac58b9f2ee2590
Hossein Samani <samanihossein1379@gmail.com>
2022-05-03 21:09:27 +0430

    Result component implemented ( still needs improvments )

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="result">
-    <div class="title"></div>
-    <div class="desc"></div>
+    <div class="title">{{ finalResult.title }}</div>
+    <div class="desc">{{ finalResult.desc }}</div>
   </div>
 </template>
 
-<script setup>
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    score: {
+      type: String,
+      default: "0",
+    },
+  },
 
+  setup(props) {
+    const results = [
+      {
+        title: "congrats!!",
+        desc: "Your studies have finally paid off!",
+      },
+      {
+        title: "ops!!",
+        desc: "No Problem, learn from today, try again tomorrow!",
+      },
+    ];
+    const _score = computed(() => props.score);
+
+    const finalResult = computed(() =>
+      _score.value > 66 ? results[0] : results[1]
+    );
+
+    return {
+      finalResult,
+    };
+  },
+};
 </script>
 
 <style></style>
