5cb3314aba37cae1e5602c8a860ef15712c62493
Hossein Samani <samanihossein1379@gmail.com>
2022-05-06 11:48:29 +0430

    ProgressBar component needs to be compelete

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="progress-bar">
+    <div class="progress">
+      {{}} questions of out {{}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "ProgressBarComponent",
+  props: ["questionsCount", "AnsweredQuestionsCount"],
+  setup(props) {
+    const progressWidth = computed(
+      () => (props.AnsweredQuestionsCount / props.questionsCount) * 100
+    );
+
+    return {
+      progressWidth,
+    };
+  },
+};
+</script>
+
+<style></style>
